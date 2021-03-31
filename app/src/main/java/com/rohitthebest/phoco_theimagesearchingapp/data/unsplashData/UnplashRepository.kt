3ac58dd7bac1b6ccd56dc8c5d1419713b44112ed
Rohit <rkumar8092378845@gmail.com>
2021-03-31 15:18:58 +0530

    -> Defined UnplashRepository.kt

--- app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/data/unsplashData/UnplashRepository.kt
+++ app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/data/unsplashData/UnplashRepository.kt
@@ -1,8 +1,23 @@
 package com.rohitthebest.phoco_theimagesearchingapp.data.unsplashData
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import com.rohitthebest.phoco_theimagesearchingapp.api.UnsplashAPI
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class UnplashRepository {
+class UnplashRepository @Inject constructor(
+        private val unsplashApi: UnsplashAPI
+) {
+
+    fun getSearchResultsFromUnsplash(query: String) = Pager(
+
+            config = PagingConfig(
+                    pageSize = 20,
+                    maxSize = 100
+            ),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashApi, query) }
+    ).flow
 
 }
