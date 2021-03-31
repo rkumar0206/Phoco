7e2bde60858c9fb73483d6bfb23b2a8c16fdcd98
Rohit <rkumar8092378845@gmail.com>
2021-03-31 23:12:24 +0530

    -> Made partial layout for the adapter_home_recyclerview.xml

--- app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/ui/fragments/HomeFragment.kt
@@ -1,7 +1,26 @@
 package com.rohitthebest.phoco_theimagesearchingapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.rohitthebest.phoco_theimagesearchingapp.R
+import com.rohitthebest.phoco_theimagesearchingapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentHomeBinding.bind(view)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
 }
