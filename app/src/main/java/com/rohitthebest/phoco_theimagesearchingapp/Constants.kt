83dee41dc08f68eb7d4b360a9980722c2dbe7de6
Rohit <rkumar8092378845@gmail.com>
2021-03-31 12:58:14 +0530

    -> Made an interface for getting the unpslash response

---
+++ app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/Constants.kt
@@ -0,0 +1,7 @@
+package com.rohitthebest.phoco_theimagesearchingapp
+
+object Constants {
+
+    const val UNSPLASH_BASE_URL = "https://api.unsplash.com/"
+
+}
---
+++ app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/api/UnsplashAPI.kt
@@ -0,0 +1,48 @@
+package com.rohitthebest.phoco_theimagesearchingapp.api
+
+import com.rohitthebest.phoco_theimagesearchingapp.BuildConfig.UNSPLASH_CLIENT_ID
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+
+interface UnsplashAPI {
+
+    //for getting the list of photos
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $UNSPLASH_CLIENT_ID")
+    @GET("photos")
+    suspend fun getPhotos(
+            @Query("page") page: Int,
+            @Query("per_page") perPage: Int,
+            @Query("order_by") orderBy: String = "latest"
+    ): UnsplashResponse
+
+
+    //Retrieving single photo using id
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $UNSPLASH_CLIENT_ID")
+    @GET("photos/:id")
+    suspend fun getPhotoByID(
+            @Query("id") id: String
+    ): UnsplashResponse
+
+
+    //getting photos related to search query
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $UNSPLASH_CLIENT_ID")
+    @GET("search/photos")
+    suspend fun searchPhoto(
+            @Query("query") query: String,
+            @Query("page") page: Int,
+            @Query("per_page") perPage: Int,
+            @Query("order_by") orderBy: String = "latest"
+    ): UnsplashResponse
+
+
+    //getting random photos
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $UNSPLASH_CLIENT_ID")
+    @GET("photos/random")
+    suspend fun getRandomPhotos(
+            @Query("count") count: Int = 30
+    ): UnsplashResponse
+
+
+}
