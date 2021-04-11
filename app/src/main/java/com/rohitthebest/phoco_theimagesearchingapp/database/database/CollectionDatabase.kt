f0cd7cdcaeecbce1b952a5e1cb0d35de748b3142
Rohit <rkumar8092378845@gmail.com>
2021-04-11 13:54:01 +0530

    -> Defined the CollectionDatabase.kt

---
+++ app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/database/database/CollectionDatabase.kt
@@ -0,0 +1,12 @@
+package com.rohitthebest.phoco_theimagesearchingapp.database.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.rohitthebest.phoco_theimagesearchingapp.database.dao.CollectionDao
+import com.rohitthebest.phoco_theimagesearchingapp.database.entity.Collection
+
+@Database(entities = [Collection::class], version = 1)
+abstract class CollectionDatabase : RoomDatabase() {
+
+    abstract fun getCollectionDao(): CollectionDao
+}
