71e97eab3fa20d2dfbd64e65ef61e9e4ac2aa4cd
Rohit <rkumar8092378845@gmail.com>
2021-04-13 20:30:15 +0530

    -> Defined app navigation for ChooseFromCollectionsFragment.kt

--- app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/ui/fragments/dialogFragments/ChooseFromCollectionsFragment.kt
+++ app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/ui/fragments/dialogFragments/ChooseFromCollectionsFragment.kt
@@ -6,22 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.rohitthebest.phoco_theimagesearchingapp.R
-import com.rohitthebest.phoco_theimagesearchingapp.databinding.FragmentChooseFromCaollectionsBinding
+import com.rohitthebest.phoco_theimagesearchingapp.databinding.FragmentChooseFromCollectionsBinding
 
 class ChooseFromCollectionsFragment : BottomSheetDialogFragment() {
 
-    private var _binding: FragmentChooseFromCaollectionsBinding? = null
+    private var _binding: FragmentChooseFromCollectionsBinding? = null
     private val binding get() = _binding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_choose_from_caollections, container, false)
+        return inflater.inflate(R.layout.fragment_choose_from_collections, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        _binding = FragmentChooseFromCaollectionsBinding.bind(view)
+        _binding = FragmentChooseFromCollectionsBinding.bind(view)
 
 
     }
