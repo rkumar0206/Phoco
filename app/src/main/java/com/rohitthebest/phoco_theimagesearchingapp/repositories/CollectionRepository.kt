ba4cc33b1f97d36a01641e0f058cfc9e7747b346
Rohit <rkumar8092378845@gmail.com>
2021-04-14 12:07:14 +0530

    -> Defined CollectionRepository.kt

---
+++ app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/repositories/CollectionRepository.kt
@@ -0,0 +1,22 @@
+package com.rohitthebest.phoco_theimagesearchingapp.repositories
+
+import com.rohitthebest.phoco_theimagesearchingapp.database.dao.CollectionDao
+import com.rohitthebest.phoco_theimagesearchingapp.database.entity.Collection
+import javax.inject.Inject
+
+class CollectionRepository @Inject constructor(
+        val dao: CollectionDao
+) {
+
+    suspend fun insertCollection(collection: Collection) = dao.insertCollection(collection)
+
+    suspend fun insertCollections(collections: List<Collection>) = dao.insertCollections(collections)
+
+    suspend fun deleteCollection(collection: Collection) = dao.deleteCollection(collection)
+
+    suspend fun deleteAllCollection() = dao.deleteAllCollection()
+
+    fun getAllCollection() = dao.getAllCollections()
+
+    fun getCollectionByKet(key: String) = dao.getCollectionByKey(key)
+}
