48cb07e385584050b1d24f545504bd7661bb7f1f
Rohit <rkumar8092378845@gmail.com>
2021-04-14 12:03:03 +0530

    -> Defined SavedImageRepository.kt

---
+++ app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/repositories/SavedImageRepository.kt
@@ -0,0 +1,23 @@
+package com.rohitthebest.phoco_theimagesearchingapp.repositories
+
+import com.rohitthebest.phoco_theimagesearchingapp.database.dao.SavedImagesDao
+import com.rohitthebest.phoco_theimagesearchingapp.database.entity.SavedImage
+import javax.inject.Inject
+
+class SavedImageRepository @Inject constructor(
+        val dao: SavedImagesDao
+) {
+
+    suspend fun insertImage(savedImage: SavedImage) = dao.insertImage(savedImage)
+
+    suspend fun insertImages(savedImages: List<SavedImage>) = dao.insertImages(savedImages)
+
+    suspend fun deleteImage(savedImage: SavedImage) = dao.delete(savedImage)
+
+    suspend fun deleteAllImage() = dao.deleteAll()
+
+    fun getAllSavesImages() = dao.getAllSavedImages()
+
+    fun getSavedImagesByCollectionKey(collectionKey: String) = dao.getAllSavedImagesByCollectionKey(collectionKey)
+
+}
