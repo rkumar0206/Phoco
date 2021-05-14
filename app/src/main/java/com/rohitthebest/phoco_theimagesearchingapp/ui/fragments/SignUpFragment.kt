74be09a051634ec564d367116ffceb06b0373efd
Rohit <rkumar8092378845@gmail.com>
2021-05-14 13:12:25 +0530

    -> Made SignUpFragment.kt

---
+++ app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/ui/fragments/SignUpFragment.kt
@@ -0,0 +1,29 @@
+package com.rohitthebest.phoco_theimagesearchingapp.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.rohitthebest.phoco_theimagesearchingapp.R
+import com.rohitthebest.phoco_theimagesearchingapp.databinding.FragmentSignupBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignUpFragment : Fragment(R.layout.fragment_signup) {
+
+    private var _binding: FragmentSignupBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentSignupBinding.bind(view)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+}
