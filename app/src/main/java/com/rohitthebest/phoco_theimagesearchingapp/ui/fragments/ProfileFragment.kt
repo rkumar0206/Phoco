d0d009b7f64a026968529f80a435b8635257d7be
Rohit <rkumar8092378845@gmail.com>
2021-05-15 00:20:43 +0530

    -> Checking for access tokens saved or not in ProfileFragment.kt

--- app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/rohitthebest/phoco_theimagesearchingapp/ui/fragments/ProfileFragment.kt
@@ -2,15 +2,21 @@ package com.rohitthebest.phoco_theimagesearchingapp.ui.fragments
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.rohitthebest.phoco_theimagesearchingapp.R
-import com.rohitthebest.phoco_theimagesearchingapp.api.UserResponse
+import com.rohitthebest.phoco_theimagesearchingapp.data.AuthToken
 import com.rohitthebest.phoco_theimagesearchingapp.data.Resources
 import com.rohitthebest.phoco_theimagesearchingapp.databinding.FragmentProfileBinding
+import com.rohitthebest.phoco_theimagesearchingapp.utils.getSavedAuthToken
+import com.rohitthebest.phoco_theimagesearchingapp.utils.hide
+import com.rohitthebest.phoco_theimagesearchingapp.utils.show
 import com.rohitthebest.phoco_theimagesearchingapp.viewmodels.apiViewModels.PhocoViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
+private const val TAG = "ProfileFragment"
 
 @AndroidEntryPoint
 @SuppressLint("SetTextI18n")
@@ -21,15 +27,32 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     private val phocoViewModel by viewModels<PhocoViewModel>()
 
-    private var accessToken = ""
-    private var userResponse: UserResponse? = null
+    private var authTokens: AuthToken? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         _binding = FragmentProfileBinding.bind(view)
 
+        authTokens = getSavedAuthToken(requireActivity())
 
+        if (authTokens == null) {
+
+            Log.d(TAG, "onViewCreated: auth token is null")
+
+            binding.loginCL.show()
+        } else {
+
+            Log.d(TAG, "onViewCreated: auth token is not null")
+
+            binding.loginCL.hide()
+
+            // check if last updated date for getting the tokens is not more than 15 days
+
+            // if not : refresh the token
+
+            // else show the login screen again
+        }
     }
 
     private fun observeSignUpResponse() {
